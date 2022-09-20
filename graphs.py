d921a87e6fa29692a4525db84dbf9d966ba7901c
Guilherme <guialbquerque@gmail.com>
2022-09-20 20:17:58 -0300

    Added more vertexes to complement the class graph algorithm of go through one city to another

--- graphs.py
+++ graphs.py
@@ -3,6 +3,7 @@ class Adjacent:
         self.vertex = vertex
         self.cust = cust
 
+
 class Vertex:
     def __init__(self, label):
         self.label = label
@@ -22,9 +23,64 @@ class Graphs:
     oradea = Vertex("Oradea")
     zerind = Vertex("Zerind")
     sibiu = Vertex("Sibiu")
+    arad = Vertex("Arad")
+    timisoara = Vertex("Timisoara")
+    lugoj = Vertex("Lugoj")
+    mehadia = Vertex("Mehadia")
+    dobreta = Vertex("Dobreta")
+    craiova = Vertex("Craiova")
+    rimnicu_vilcea = Vertex("Rimnicu Vilcea")
+    fagaras = Vertex("Fagaras")
+    pitesti = Vertex("Pitesti")
+    bucharest = Vertex("Bucharest")
+    giurgiu = Vertex("Giurgiu")
+    urziceni = Vertex("Urziceni")
 
     oradea.add_adjacents(Adjacent(zerind, 71))
     oradea.add_adjacents(Adjacent(sibiu, 151))
+
+    zerind.add_adjacents(Adjacent(arad, 75))
+    zerind.add_adjacents(Adjacent(oradea, 71))
+
+    arad.add_adjacents(Adjacent(zerind, 75))
+    arad.add_adjacents(Adjacent(sibiu, 140))
+    arad.add_adjacents(Adjacent(timisoara, 118))
+
+    timisoara.add_adjacents(Adjacent(arad, 118))
+    timisoara.add_adjacents(Adjacent(lugoj, 111))
+
+    lugoj.add_adjacents(Adjacent(timisoara, 111))
+    lugoj.add_adjacents(Adjacent(mehadia, 70))
+
+    mehadia.add_adjacents(Adjacent(lugoj, 70))
+    mehadia.add_adjacents(Adjacent(dobreta, 75))
+
+    dobreta.add_adjacents(Adjacent(mehadia, 75))
+    dobreta.add_adjacents(Adjacent(craiova, 120))
+
+    sibiu.add_adjacents(Adjacent(arad, 140))
+    sibiu.add_adjacents(Adjacent(oradea, 151))
+    sibiu.add_adjacents(Adjacent(fagaras, 99))
+    sibiu.add_adjacents(Adjacent(rimnicu_vilcea, 80))
+
+    rimnicu_vilcea.add_adjacents(Adjacent(sibiu, 80))
+    rimnicu_vilcea.add_adjacents(Adjacent(craiova, 146))
+    rimnicu_vilcea.add_adjacents(Adjacent(pitesti, 97))
+
+    craiova.add_adjacents(Adjacent(rimnicu_vilcea, 146))
+    craiova.add_adjacents(Adjacent(dobreta, 120))
+    craiova.add_adjacents(Adjacent(pitesti, 138))
+
+    pitesti.add_adjacents(Adjacent(craiova, 138))
+    pitesti.add_adjacents(Adjacent(rimnicu_vilcea, 97))
+    pitesti.add_adjacents(Adjacent(bucharest, 101))
+
+    fagaras.add_adjacents(Adjacent(sibiu, 99))
+    fagaras.add_adjacents(Adjacent(bucharest, 211))
+
+    bucharest.add_adjacents(Adjacent(fagaras, 211))
+    bucharest.add_adjacents(Adjacent(pitesti, 101))
+    bucharest.add_adjacents(Adjacent(urziceni, 85))
 
 
 graphs = Graphs()
