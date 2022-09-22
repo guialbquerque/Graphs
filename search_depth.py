c67efb7cf359151aff417208955fd50729cc5386
Guilherme <guialbquerque@gmail.com>
2022-09-22 01:32:05 -0300

    Implemented the class Depth Search and made some tests

---
+++ search_depth.py
@@ -0,0 +1,26 @@
+from graphs_udemy import Graphs
+from stack_udemy import Stack
+
+class DepthSearch:
+    def __init__(self, start, number_of_elements):
+        self.start = start
+        self.start.visited = True
+        self.stack = Stack(number_of_elements)
+        self.stack.Stack_Up(start)
+    
+    def search(self):
+        top = self.stack.view_top()
+        print(f'Top of stack: {top.label}')
+        for adj in top.adjacents:
+            print(f'Top is: {top. label}. Is {adj.vertex.label} already visited? {adj.vertex.visited}')
+            if adj.vertex.visited == False:
+                adj.vertex.visited = True
+                self.stack.Stack_Up(adj.vertex)
+                print(f'Stacked : {adj.vertex.label}')
+                self.search()
+        print(f'Unstacked: {self.stack.Unstack().label}')
+        print()
+
+graphs = Graphs()
+deph_search = DepthSearch(graphs.arad, 20)
+deph_search.search()
