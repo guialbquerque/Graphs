6d417d681b505e34b5b89b469fc99e07652b2051
Guilherme <guialbquerque@gmail.com>
2022-10-10 02:12:32 -0300

    Method first_value deleted

--- Intelligent Algorithms/ordered_vector.py
+++ Intelligent Algorithms/ordered_vector.py
@@ -6,7 +6,7 @@ class OrderedVector:
     def __init__(self, capacity):
         self._capacity = capacity
         self._lastPosition = -1
-        self._values = np.empty(self._capacity, dtype = object)
+        self.values = np.empty(self._capacity, dtype = object)
 
     # O(N)
     def printer(self):
@@ -14,33 +14,34 @@ class OrderedVector:
             print("Vector is empty!")
         else:
             for i in range(self._lastPosition + 1):
-                print(f"Position: {i} - Element: {self._values[i]. label} - Distance: {self._values[i]. distance}")
+                print(f"Position: {i} - Element: {self.values[i].label} - Distance: {self.values[i].distance}")
 
     def insert(self, value):
-        if self._lastPosition == len(self._values) - 1:
+        if self._lastPosition == len(self.values) - 1:
             print("The vector is full, max capacity reached!")
         else:
             position = 0
             for i in range(self._lastPosition + 1):
                 position = i
-                if self._values[i].distance > value.distance:
+                if self.values[i].distance > value.distance:
                     break
                 elif i == self._lastPosition:
                     position += 1
 
             x = self._lastPosition
             while x >= position:
-                self._values[x + 1] = self._values[x]
+                self.values[x + 1] = self.values[x]
                 x -= 1
 
-            self._values[position] = value
+            self.values[position] = value
             self._lastPosition += 1
+
 
 if __name__ == "__main__":
     graphs = Graphs()
 
     vector = OrderedVector(5)
-    vector.insert(graphs.arad)
+    vector.insert(Graphs().arad)
     vector.insert(graphs.oradea)
     vector.insert(graphs.sibiu)
     vector.insert(graphs.timisoara)
